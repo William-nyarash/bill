1bcb59b453cdb4d2af299f9967c5a45d41a13bf8
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-04-05 20:37:41 +0300

    reinitialize the project

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*
-  Author: Alvin Kiveu
-  Description: Mpesa Daraja API with Node JS
-  Date: 23/10/2023
-  Github Link: https://github.com/alvin-kiveu/Mpesa-Daraja-Api-NODE.JS.git
-  Website: www.umeskiasoftwares.com
-  Email: info@umeskiasoftwares.com
-  Phone: +254113015674
-  
-*/
-
 const express = require("express");
 const app = express();
 // const http = require("http");
@@ -32,9 +21,8 @@ app.use('/', apiRouter);
 
 // ACCESS TOKEN FUNCTION - Updated to use 'axios'
 async function getAccessToken() {
-  const consumer_key = "BGdiWF6Wva7PMvyEqfJhlj4c8w0sDj6u926mP9Ax92HS6OoO"; // REPLACE IT WITH YOUR CONSUMER KEY
-  const consumer_secret = "Lkub3bYKjZAaEsAuKw5VvJ7dMJF1F1RFJYAQAAlQNtIBpRH1ovfOCkiVtDCe8TQc"; // REPLACE IT WITH YOUR CONSUMER SECRET
-
+  const consumer_key = process.env.CONSUMER_KEY
+  const consumer_secret = process.env.CONSUMER_SECRET
   const url =
     "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
   const auth =
@@ -58,165 +46,11 @@ async function getAccessToken() {
 }
 
 app.get("/", (req, res) => {
-  res.send("MPESA DARAJA API WITH NODE JS BY UMESKIA SOFTWARES");
+  res.send("banking revolutionized");
   var timeStamp = moment().format("YYYYMMDDHHmmss");
   console.log(timeStamp);
 });
-
-
-//ACCESS TOKEN ROUTE
-app.get("/access_token", (req, res) => {
-  getAccessToken()
-    .then((accessToken) => {
-      res.send("😀 Your access token is " + accessToken);
-    })
-    .catch(console.log);
-});
-
-//MPESA STK PUSH ROUTE
-app.get("/stkpush", (req, res) => {
-  getAccessToken()
-    .then((accessToken) => {
-      const url =
-        "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
-      const auth = "Bearer " + accessToken;
-      const timestamp = moment().format("YYYYMMDDHHmmss");
-      const password = new Buffer.from(
-        "174379" +
-          "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" +
-          timestamp
-      ).toString("base64");
-
-      axios
-        .post(
-          url,
-          {
-            BusinessShortCode: "174379",
-            Password: password,
-            Timestamp: timestamp,
-            TransactionType: "CustomerPayBillOnline",
-            Amount: "10",
-            PartyA: "0114692825", //phone number to receive the stk push
-            PartyB: "174379",
-            PhoneNumber: "",
-            CallBackURL: "https://dd3d-105-160-22-207.ngrok-free.app/callback",
-            AccountReference: "UMESKIA PAY",
-            TransactionDesc: "Mpesa Daraja API stk push test",
-          },
-          {
-            headers: {
-              Authorization: auth,
-            },
-          }
-        )
-        .then((response) => {
-          res.send("😀 Request is successful done ✔✔. Please enter mpesa pin to complete the transaction");
-        })
-        .catch((error) => {
-          console.log(error);
-          res.status(500).send("❌ Request failed");
-        });
-    })
-    .catch(console.log);
-});
-
-//STK PUSH CALLBACK ROUTE
-app.post("/callback", (req, res) => {
-  console.log("STK PUSH CALLBACK");
-  const CheckoutRequestID = req.body.Body.stkCallback.CheckoutRequestID;
-  const ResultCode = req.body.Body.stkCallback.ResultCode;
-  var json = JSON.stringify(req.body);
-  fs.writeFile("stkcallback.json", json, "utf8", function (err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("STK PUSH CALLBACK JSON FILE SAVED");
-  });
-  console.log(req.body);
-});
-
-// REGISTER URL FOR C2B
-app.get("/registerurl", (req, resp) => {
-  getAccessToken()
-    .then((accessToken) => {
-      const url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl";
-      const auth = "Bearer " + accessToken;
-      axios
-        .post(
-          url,
-          {
-            ShortCode: "174379",
-            ResponseType: "Complete",
-            ConfirmationURL: "http://example.com/confirmation",
-            ValidationURL: "http://example.com/validation",
-          },
-          {
-            headers: {
-              Authorization: auth,
-            },
-          }
-        )
-        .then((response) => {
-          resp.status(200).json(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-          resp.status(500).send("❌ Request failed");
-        });
-    })
-    .catch(console.log);
-});
-
-app.get("/confirmation", (req, res) => {
-  console.log("All transaction will be sent to this URL");
-  console.log(req.body);
-});
-
-app.get("/validation", (req, resp) => {
-  console.log("Validating payment");
-  console.log(req.body);
-});
-
-// B2C ROUTE OR AUTO WITHDRAWAL
-app.get("/b2curlrequest", (req, res) => {
-  getAccessToken()
-    .then((accessToken) => {
-      const securityCredential =
-        "N3Lx/hisedzPLxhDMDx80IcioaSO7eaFuMC52Uts4ixvQ/Fhg5LFVWJ3FhamKur/bmbFDHiUJ2KwqVeOlSClDK4nCbRIfrqJ+jQZsWqrXcMd0o3B2ehRIBxExNL9rqouKUKuYyKtTEEKggWPgg81oPhxQ8qTSDMROLoDhiVCKR6y77lnHZ0NU83KRU4xNPy0hRcGsITxzRWPz3Ag+qu/j7SVQ0s3FM5KqHdN2UnqJjX7c0rHhGZGsNuqqQFnoHrshp34ac/u/bWmrApUwL3sdP7rOrb0nWasP7wRSCP6mAmWAJ43qWeeocqrz68TlPDIlkPYAT5d9QlHJbHHKsa1NA==";
-      const url = "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest";
-      const auth = "Bearer " + accessToken;
-      axios
-        .post(
-          url,
-          {
-            InitiatorName: "testapi",
-            SecurityCredential: securityCredential,
-            CommandID: "PromotionPayment",
-            Amount: "1",
-            PartyA: "600996",
-            PartyB: "",//phone number to receive the stk push
-            Remarks: "Withdrawal",
-            QueueTimeOutURL: "https://mydomain.com/b2c/queue",
-            ResultURL: "https://mydomain.com/b2c/result",
-            Occasion: "Withdrawal",
-          },
-          {
-            headers: {
-              Authorization: auth,
-            },
-          }
-        )
-        .then((response) => {
-          res.status(200).json(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-          res.status(500).send("❌ Request failed");
-        });
-    })
-    .catch(console.log);
-});
-
+// work in progress
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
